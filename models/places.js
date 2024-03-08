75a7e1d49a93c03a845399b0637dee26c59b441c
ambscara <150319615+ambscara@users.noreply.github.com>
2024-03-07 20:52:01 -0800

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: 'public/images/max-griss-otLqpb9LK70-unsplash (1) copy.jpg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: 'public/images/CatCafe.jpg'
+}]
