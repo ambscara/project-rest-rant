1c9deaaa181c6a80966df0a16043af909e9fe5f4
ambscara <150319615+ambscara@users.noreply.github.com>
2024-03-20 16:15:32 -0700

    Finished adding comment section

--- rest-rant/views/places/show.jsx
+++ rest-rant/views/places/show.jsx
@@ -1,7 +1,28 @@
 const React = require('react');
 const Def = require('../default');
 
-function show (data) {
+function Show (data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    );
+
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😄'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            );
+        });
+    }
+
     return (
         <Def>
             <main className="container">
@@ -31,27 +52,46 @@ function show (data) {
                         </div>
                         <div>
                             <h3>Comments:</h3>
-                            <p>No comments yet!</p>
+                            {comments}
                         </div>
                     </div>
                     <div className="col-md-6">
                         <img src={data.place.pic} alt={data.place.name} className="img-fluid" />
                     </div>
                     <div>
-                    <a href={'/places/${data.id}/edit'} className="btn btn-warning"> 
-                        Edit
-                    </a>  
-                    <form method="POST" action={'/places/${data.id}}?_method=DELETE'}> 
-                      <button type="submit" className="btn btn-danger">
-                        Delete
-                      </button>
-                    </form>     
-
-                   </div>
+                        <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                            Edit
+                        </a>  
+                        <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                            <button type="submit" className="btn btn-danger">
+                                Delete
+                            </button>
+                        </form>     
+                    </div>
                 </div>
+                <h2>Add a Comment</h2>
+                <form action="/submit_comment" method="POST">
+                    <div className="form-group">
+                        <label htmlFor="author">Author:</label>
+                        <input type="text" className="form-control" id="author" name="author" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="content">Content:</label>
+                        <textarea className="form-control" id="content" name="content"></textarea>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="star_rating">Star Rating:</label>
+                        <input type="number" className="form-control" id="star_rating" name="star_rating" step="0.5" />
+                    </div>
+                    <div className="form-group form-check">
+                        <input type="checkbox" className="form-check-input" id="rant" name="rant" />
+                        <label className="form-check-label" htmlFor="rant">Rant</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
             </main>
         </Def>
     );
 }
 
-module.exports = show;
+module.exports = Show;
